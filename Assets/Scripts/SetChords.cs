86778e555daa0715309acddaec24d99be163fa54
Christian Laverde <55038118+CrisBlu@users.noreply.github.com>
2025-06-02 20:31:20 -0400

    comment

--- Assets/Scripts/SetChords.cs
+++ Assets/Scripts/SetChords.cs
@@ -5,4 +5,6 @@ public class SetChords
     private string[] majorScale = new string[] { "E", "F#", "G#", "A", "B", "C#", "D#", "E" };
     private string[] minorScale = new string[] { "E", "F", "G#", "A", "B", "C", "D", "E" };
     private string[] pentatonicScale = new string[] { "E", "F#", "G#","B","E" };
+
+    ///Compare notes from arrays with names of the note game objects; highlight the applicable notes; maybe do a blue outline?
 }
